454fe3170f9c9c1a6ac109078df547dd54f5e49d
Shubham_Satyam <pioneer9101993@gmail.com>
2024-07-23 00:50:33 +0530

    Added config reads

--- src/invoker.py
+++ src/invoker.py
@@ -1,13 +1,19 @@
 from pyspark.sql import SparkSession, DataFrame
-from pathlib import Path
 import logging
+import configparser as cp
+from datetime import datetime
 
 
 class Invoker:
 
     def __init__(self):
+        self.config_parser = cp.ConfigParser()
+        self.config_parser.optionxform = str
+        self.config_parser.read('/Users/shubhamsatyam/Downloads/projects/DSA/project.properties')
+        self._input_path = self.config_parser.get('COMMON_SECTION', 'input_path')
+        self._output_path = self.config_parser.get('COMMON_SECTION', 'output_path')
+        self._timestamp_path = self.config_parser.get('COMMON_SECTION', 'timestamp_path')
         self._spark = None
-        self.path = str(Path().absolute()).split("src")[0]+"/hello.csv"
 
     def __enter__(self):
         self._spark = SparkSession.builder.getOrCreate()
@@ -18,15 +24,24 @@ class Invoker:
         self._spark.sparkContext.stop()
 
     def read_csv(self) -> DataFrame:
-        df = self._spark.read.option("header", "True").csv(self.path)
+        df = self._spark.read.option("header", "True").csv(self._input_path)
         return df
 
     def write_df(self, df: DataFrame) -> None:
-        df.repartition(1).write.format("csv").save(self.path.split("hello.csv")[0]+"op")
+        df.repartition(1).write.format("csv").mode('overwrite').save(self._output_path)
+        self.write_current_timestamp()
 
     def do(self) -> None:
         df = self.read_csv()
         self.write_df(df)
+
+    def read_last_timestamp(self, path):
+        return self._spark.read.option("header", "False").csv(path).collect()[0][0]
+
+    def write_current_timestamp(self) -> None:
+        timestamp = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+        df = self._spark.createDataFrame([(timestamp,)], ["timestamp"])
+        df.repartition(1).write.format("csv").mode('overwrite').save(self._timestamp_path)
 
 
 if __name__ == '__main__':
